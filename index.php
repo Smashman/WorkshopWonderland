174714bf58d3a5d15ca3fa828ea37ae0a14c1e4b
Ben <ben@smash.mn>
2015-12-22 04:27:50 +0000

    Enabled Google Analytics

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
         <title>Workshop Wonderland</title>
         <link rel="stylesheet" type="text/css" media="screen" href="css/main.css">
         <link rel="shortcut icon" href="favicon.ico">
-<!--        <script>-->
-<!--            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){-->
-<!--                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),-->
-<!--                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)-->
-<!--            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');-->
-<!---->
-<!--            ga('create', 'UA-53670757-2', 'auto');-->
-<!--            ga('send', 'pageview');-->
-<!---->
-<!--        </script>-->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-53670757-3', 'auto');
+            ga('send', 'pageview');
+
+        </script>
     </head>
     <body>
         <div class="container" id="container_01">
